a639de234e753026946fffbe22cc8ee837068a6e
Loka <lokapriyaraoo@gmail.com>
2026-09-25 03:12:10 +0530

    Create  app/prescription.tsx

---
+++  app/prescription.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function PrescriptionScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>
+        Prescription Entry
+      </Text>
+    </View>
+  );
+}
